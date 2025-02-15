c831380dab2f163914c571e1ca86b1ede10022d5
zulhaidir <zulhaidirm11@gmail.com>
2025-02-15 13:57:45 +0800

    test terus saja

--- mantap/db/sqlc/entry_test.go
+++ mantap/db/sqlc/entry_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/Zulhaidir/microservice/mantap/util"
 	"github.com/stretchr/testify/require"
-	"github.com/sulelearning/mantap/mantap/util"
 )
 
 func createRandomEntry(t *testing.T, account Account) Entry {
