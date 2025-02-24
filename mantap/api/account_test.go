cdaa3d84d28275ee39e2bab912885ef9721590bd
zulhaidir <zulhaidirm11@gmail.com>
2025-02-24 23:49:37 +0800

    Fix testing TestAccountGetAPI

--- mantap/api/account_test.go
+++ mantap/api/account_test.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -37,6 +40,7 @@ func TestAccountGetAPI(t *testing.T) {
 	server.router.ServeHTTP(recorder, request)
 	/* check response */
 	require.Equal(t, http.StatusOK, recorder.Code)
+	requireBodyMatchAccount(t, recorder.Body, account)
 
 }
 
@@ -47,4 +51,14 @@ func randomAccount() db.Account {
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
+}
+
+func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Account) {
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccount db.Account
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t, account, gotAccount)
 }
