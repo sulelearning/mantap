9eb47d29cdb430df12a4828d5bfc61b4e8cd8712
zulhaidir <zulhaidirm11@gmail.com>
2025-03-17 03:16:36 +0800

    Implementasi gRPC API untuk create dan login user

---
+++ mantap/grpcapi/converter.go
@@ -0,0 +1,17 @@
+package grpcapi
+
+import (
+	db "github.com/Zulhaidir/microservice/mantap/db/sqlc"
+	"github.com/Zulhaidir/microservice/mantap/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		Fullname:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
---
+++ mantap/grpcapi/rpc_create_user.go
@@ -0,0 +1,42 @@
+package grpcapi
+
+import (
+	"context"
+
+	db "github.com/Zulhaidir/microservice/mantap/db/sqlc"
+	"github.com/Zulhaidir/microservice/mantap/pb"
+	"github.com/Zulhaidir/microservice/mantap/util"
+	"github.com/lib/pq"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	hashedPassword, err := util.HashPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.GetUsername(),
+		HashedPassword: hashedPassword,
+		FullName:       req.GetFullname(),
+		Email:          req.GetEmail(),
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqError, ok := err.(*pq.Error); ok {
+			switch pqError.Code.Name() {
+			case "unique_violation":
+				return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
+			}
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	rsp := &pb.CreateUserResponse{
+		User: convertUser(user),
+	}
+	return rsp, nil
+}
